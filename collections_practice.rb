1e90dd7e757c5bbde0659946bd411315b0222416
teskew <teskinder1@gmail.com>
2020-11-20 16:44:42 -0600

    commit tree bbbc7fd3b971349fa27ef4b6122b6c3fad91964e

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,49 @@
+def sort_array_asc(array)
+    array.sort     
+end
+def sort_array_desc(array)
+    array.sort do | a, b|
+        b <=> a
+    end
+end
+def sort_array_char_count(array)
+    
+    array.sort do | a, b|
+        a.length <=> b.length
+    end
+end 
+def swap_elements(array) 
+  
+    array[1], array[2] = array[2], array[1]
+    array
+    end
+ def reverse_array(array)
+        array.reverse
+    end
+ def kesha_maker(array)
+    array.each do |character|
+        character[2] = "$"
+ end
+ end
+ def find_a(array)
+      array.find_all do |word|
+        word[0] == "a"
+      end
+ end
+ def sum_array(array)
+    sum = 0
+     array.each do |number|
+        sum += number
+     end
+     sum
+ end
+ def add_s(array)
 
+    array.collect do |word|
+    if array[1] == word
+        word
+    else
+        word + "s"
+    end
+end
+ end
